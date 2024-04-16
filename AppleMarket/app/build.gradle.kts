bf0650b3f3272a15fe4bc56c2d3e19997d015baa
kang <guswjd9994@duksung.ac.kr>
2024-04-16 20:17:00 +0900

    [feat] 사과마켓 - 아이템 상세 페이지 구현

--- AppleMarket/app/build.gradle.kts
+++ AppleMarket/app/build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    // 원형 이미지뷰
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation(libs.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- AppleMarket/app/src/main/java/com/example/applemarket/DetailActivity.kt
+++ AppleMarket/app/src/main/java/com/example/applemarket/DetailActivity.kt
@@ -1,4 +1,71 @@
 package com.example.applemarket
 
-class DetailActivity {
+import android.content.Intent
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.applemarket.databinding.ActivityDetailBinding
+import com.google.android.material.snackbar.Snackbar
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    private var isLiked = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        setData()
+        setUnderLineText()
+        binding.btnBack.setOnClickListener {
+            onBackPressed()
+        }
+        binding.ivLikeBtn.setOnClickListener {
+            addWishList()
+        }
+    }
+
+    private fun setData() {
+        val clickedItem = intent.getParcelableExtra<MarketItem>("clickedItem")
+        clickedItem?.let {
+            binding.ivDetailItem.setImageResource(it.itemImage)
+            binding.tvDetailTitle.text = it.itemTitle
+            binding.tvDetailContent.text = it.itemContent
+            binding.tvDetailPrice.text = it.itemPrice
+            binding.tvUserName.text = it.userName
+            binding.tvUserAddress.text = it.userAddress
+            isLiked = it.isLiked == true
+            binding.ivLikeBtn.setImageResource(
+                if (isLiked) {R.drawable.ic_like_full}
+                else {R.drawable.ic_like_empty}
+            )
+        }
+    }
+
+    // 텍스트 밑줄 그리기
+    private fun setUnderLineText() {
+        binding.tvMannerTemperature.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+    }
+
+    private fun addWishList() {
+        if(!isLiked){
+            binding.ivLikeBtn.setImageResource(R.drawable.ic_like_full)
+            Snackbar.make(binding.root, getString(R.string.add_wishlist), Snackbar.LENGTH_SHORT).show()
+            isLiked = true
+        }else {
+            binding.ivLikeBtn.setImageResource(R.drawable.ic_like_empty)
+            isLiked = false
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
 }
